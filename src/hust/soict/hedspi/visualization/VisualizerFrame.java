7f0b98e1b148cc7b8b7fa79798d262060d5fb7a6
thangnkHust <thangnk.hust@gmail.com>
2020-05-07 23:15:26 +0700

    refactor + create base package in project

---
+++ src/hust/soict/hedspi/visualization/VisualizerFrame.java
@@ -0,0 +1,38 @@
+package hust.soict.hedspi.visualization;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class VisualizerFrame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VisualizerFrame frame = new VisualizerFrame();
+					frame.setExtendedState(JFrame.MAXIMIZED_BOTH); //set JFrame full screen
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VisualizerFrame() {
+		
+	}
+
+}
