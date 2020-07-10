8c32674a0dd8052cbccf1dbd66e920a80de3a1a3
thangnkHust <thangnk.hust@gmail.com>
2020-07-10 14:10:14 +0700

    update ISort

--- src/hust/soict/hedspi/visualization/algotrithms/ISortAlgoritms.java
+++ src/hust/soict/hedspi/visualization/algotrithms/ISortAlgoritms.java
@@ -8,6 +8,7 @@ import hust.soict.hedspi.visualization.action.Element;
 import hust.soict.hedspi.visualization.action.PointRun;
 
 public interface ISortAlgoritms {
+	public final int a = 5;
 	public void addCode(DefaultListModel<String> model);
 	public void sortIncrease(JPanel pnImitiate, Element[] elementBoxs, PointRun pointRun);
 	public void sortDecrease(JPanel pnImitiate, Element[] elementBoxs, PointRun pointRun);
